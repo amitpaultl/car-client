015b4f076a01923415c12811a4e01dbbdf402e7d
amitpaultl <amitpaultl@gmail.com>
2022-11-25 15:36:40 +0600

    My product delete button add

--- src/Commponts/DashBord/SellerProduct/SellerProduct.js
+++ src/Commponts/DashBord/SellerProduct/SellerProduct.js
@@ -1,14 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../Context/AuthProvider';
 import './SellerProduct.css'
 
+
 const SellerProduct = () => {
 
-
+    // use context
     const { user } = useContext(AuthContext);
-    const url = `http://localhost:5000/addProduct?email=amitpaultl@gmail.com`;
-    const { data: addProduct = [] } = useQuery({
+
+    // react query data fatch
+    const url = `http://localhost:5000/addProduct?email=${user?.email}`;
+    const { data: addProduct = [], refetch } = useQuery({
         queryKey: ['addProduct', user?.email],
         queryFn: async () => {
             const res = await fetch(url, {
@@ -21,7 +25,43 @@ const SellerProduct = () => {
             return data
         }
     })
-    console.log(addProduct);
+
+// advertiseAdd 
+    const advertiseAdd = (product) => {
+        axios.post(`http://localhost:5000/addProduct/${product._id}`, {
+                publish: true
+            }
+            ,
+            {
+                headers: {
+                    "Content-type": "application/json"
+                },
+            }
+        )
+            .then(res => {
+                console.log(res);
+            })
+            .catch(data => {
+                console.log(data);
+            });
+        console.log(product._id);
+    }
+
+    // delete
+    const deleteProduct = (product)=>{
+        fetch(`http://localhost:5000/addProduct/${product._id}`,{
+            method:'DELETE',
+            headers:{
+                // authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            refetch()
+        })
+    }
+
     return (
         <div>
             <table className="table">
@@ -38,16 +78,16 @@ const SellerProduct = () => {
                 <tbody>
                     {
 
-                        addProduct?.data?.map((product ,i) =>  <tr key={i}>
-                            <th scope="row">{i+1}</th>
+                        addProduct?.data?.map((product, i) => <tr key={i}>
+                            <th scope="row">{i + 1}</th>
                             <td><img className='addProductimg' src={product?.image} alt="" /></td>
                             <td><strong>{product?.productname}</strong> </td>
-                            <td>{product?.paid ? <p class="text-secondary">Sold</p> : <p class="text-danger">Sales</p>}</td>
-                            <td>{product?.publish ? ' ' : <button type="button" class="btn btn-success">Advertise</button>}</td>
-                            <td><button type="button" class="btn btn-danger">Delete</button></td>
+                            <td>{product?.paid ? <p className="text-secondary">Sold</p> : <p className="text-danger">Sales</p>}</td>
+                            <td>{product?.publish ? ' ' : <button onClick={() => advertiseAdd(product)} type="button" className="btn btn-success">Advertise</button>}</td>
+                            <td><button onClick={()=>deleteProduct(product)} type="button" className="btn btn-danger">Delete</button></td>
                         </tr>)
                     }
-                   
+
 
                 </tbody>
             </table>
